8ccd5c112bd901985893fe904e4f0f266f198699
Ryzaxd <nick986h@edu.zealand.dk>
2024-04-10 10:39:05 +0200

    klar til fremlæggelsen

--- scrapydba/scrapydba/spiders/bilspider.py
+++ scrapydba/scrapydba/spiders/bilspider.py
@@ -1,6 +1,5 @@
 import sqlite3
 import scrapy
-import re
 from scrapy.crawler import CrawlerProcess
 from scrapy.signalmanager import dispatcher
 from scrapy import signals
@@ -23,7 +22,7 @@ class BilspiderSpider(scrapy.Spider):
         yield scrapy.Request(url=URL, callback=self.parse_front_page)
 
     def parse_front_page(self, response):
-        # Extracting URLs of each advertisement on the front page
+        # Extracting advertisement links
         ad_links = response.css('.listingLink')
         for ad_link in ad_links:
             ad_url = ad_link.css('a::attr(href)').extract_first()
@@ -35,18 +34,18 @@ class BilspiderSpider(scrapy.Spider):
             yield response.follow(next_page_link, callback=self.parse_front_page)
 
     def parse_advertisement(self, response):
-        # Extracting model name = virker
+        # Extracting model name 
         modelnavn = response.xpath('//*[@id="content"]/div[2]/article/div[4]/dl/dd[1]/text()').get()
         modelnavn = response.xpath('//*[@id="content"]/div[2]/article/div[5]/dl/dd[1]/text()').get() if not modelnavn else modelnavn
 
-        # Extracting price = virker
+        # Extracting price 
         pris = response.xpath('//div[@class="vip-heading-bar row-fluid"]/div[@class="span8"]/div/div[2]/span/text()').get()
 
-        # Extracting date = virker
+        # Extracting date 
         dato = response.xpath('//div[@class="vip-listing-info"]/span/text()').get()
         dato = dato.replace("Annonce oprettet:", "").strip() if dato else None
 
-        # Extracting location = virker nogenlunde, men der er forskellige placeringer på siden
+        # Extracting location 
         lokation_part1 = response.xpath('//*[@id="business-card"]/div[2]/div[2]/div[2]/div[1]/div/p/span[1]/text()').get()
         lokation_part1 = response.xpath('//*[@id="business-card"]/div[2]/div[2]/div[2]/div[1]/div/a/p/span[1]/text()').get() if not lokation_part1 else lokation_part1
         
@@ -55,22 +54,16 @@ class BilspiderSpider(scrapy.Spider):
         
         lokation = f"{lokation_part1.strip()} {lokation_part2.strip()}" if lokation_part1 and lokation_part2 else None
 
-        # Extracting kilometertal = virker ikke da der er forskellige placeringer på siden - hvor den blander kilometertal med modelår, farve og andre ting
+        # Extracting kilometertal 
         kilometertal = response.xpath('//*[@id="content"]/div[2]/article/div[5]/dl/dd[5]/text()').get()
-        if kilometertal:
-            match = re.search(r'\b(\d{1,3}(?:[.,\s]\d{3})+)\b', kilometertal)
-            if match:
-                kilometertal = int(match.group(1).replace('.', '').replace(',', '').replace(' ', ''))
-                if kilometertal <= 5000:
-                    return
 
-        # Extracting color = virker ikke
+        # Extracting color 
         farve = response.xpath('//*[@id="content"]/div[2]/article/div[5]/dl/dd[6]/text()').get()
 
-        # Extracting model year = virker ikke
+        # Extracting model year
         modelår = response.xpath('//*[@id="content"]/div[2]/article/div[5]/dl/dd[4]/text()').get()
 
-        # Extracting fuel type = virker
+        # Extracting fuel type 
         brændstof = response.xpath('//*[@id="content"]/div[2]/article/div[5]/dl/dd[2]/text()').get()
         brændstof = response.xpath('//*[@id="content"]/div[2]/article/div[4]/dl/dd[2]/text()').get() if not brændstof else brændstof
 
@@ -79,7 +72,7 @@ class BilspiderSpider(scrapy.Spider):
             'pris': pris.strip() if pris else None,
             'dato': dato,
             'lokation': lokation.strip() if lokation else None,
-            'kilometertal': kilometertal,
+            'kilometertal': kilometertal.strip() if kilometertal else None,
             'farve': farve.strip() if farve else None,
             'modelår': modelår.strip() if modelår else None,
             'brændstof': brændstof.strip() if brændstof else None,
